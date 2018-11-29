7508dfcac7ad5bcd532c4b705c8cbcb435c7395d
Dean Keithly <drk94@cornell.edu>
2018-11-28 22:11:17 -0500

    deleting Stellar Diameter testing... finished with ti

--- StellarDiameterTesting.py
+++
@@ -1,8 +0,0 @@
-#StellarDiameterTesting
-
-from EXOSIMS.Prototypes import StarCatalog
-from EXOSIMS.StarCatalog import EXOCAT1
-cpath = '/home/dean/Documents/exosims/EXOSIMS/EXOSIMS/StarCatalog/mission_exocat.votable'
-ec = EXOCAT1.EXOCAT1(cpath)
-
-# Can Delete This File
