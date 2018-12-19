1235a00905dff5cf97ab59b8cd86b20b7fc7c1e1
Dean Keithly <drk94@cornell.edu>
2018-12-18 23:20:34 -0500

    adding snake timeline formalizing parts of plotting function

--- DRMtoTimelinePlot.py
+++ DRMtoTimelinePlot.py
@@ -44,26 +44,34 @@ class DRMtoTimelinePlot(object):
     """
     _modtype = 'util'
 
-    def __init__(self, args):
+    def __init__(self, args=None):
         vprint(args)
+        self.args = args
         vprint('DRMtoTimelinePlot Initialization done')
         pass
 
     def singleRunPostProcessing(self, PPoutpath, folder):
         """This is called by runPostProcessing
         """
-        pass
+        if self.args == None: # Nothing was provided as input
+            # grab random pkl file from folder
+            pklfiles_in_folder = [myFileName for myFileName in os.listdir(folder) if 'run' in myFileName and '.pkl' in myFileName]  # Get names of all pkl files in path
+            pklfname = np.random.choice(pklFiles)
+            pklfile = os.path.join(folder,pklfname)
+        elif 'pklfile' in self.args.keys(): # specific pklfile was provided for analysis
+            pklfile = self.args['pklfile']
+        else: # grab random pkl file from folder
+            pklfiles_in_folder = [myFileName for myFileName in os.listdir(folder) if 'run' in myFileName and '.pkl' in myFileName]  # Get names of all pkl files in path
+            pklfname = np.random.choice(pklFiles)
+            pklfile = os.path.join(folder,pklfname)
+        outspecfile = os.path.join(folder,'outspec.json')
 
-    # if __name__ == "__main__":
-    #     parser = argparse.ArgumentParser(description="Create Mission Timeline Figures")
-    #     parser.add_argument('--pklfile', nargs=1, type=str, help='Full path to pkl file (string).')
-    #     parser.add_argument('--outspecfile', nargs=1, type=str, help='Full path to outspec file (string).')
+        self.plotTimelineWithOB(pklfile=pklfile, outspecfile=outspecfile, PPoutpath=PPoutpath)
+        self.plotSnakingTimeline(pklfile=pklfile, outspecfile=outspecfile, PPoutpath=PPoutpath)
 
-    #     args = parser.parse_args()
-    #     pklfile = args.pklfile[0]
-    #     outspecfile = args.outspecfile[0]
 
-    def PlotTimelineWithOB(self, pklfile, outspecfile, PPoutpath):
+
+    def plotTimelineWithOB(self, pklfile='./', outspecfile='./', PPoutpath='./'):
         """
         Args:
             pklfile (string) - full path to pkl file
@@ -74,48 +82,11 @@ class DRMtoTimelinePlot(object):
         #Error check to ensure provided pkl file exists
         assert os.path.isfile(pklfile), '%s not found' %pklfile
         assert os.path.isfile(outspecfile), '%s not found' %outspecfile
-
-        #if not os.path.exists(pklfile):
-        #    raise ValueError('%s not found'%pklfile)
-
-        #Given Filepath for pklfile, Plot a pkl from each testrun in subdir
-        pklPaths = list()
-        pklfname = list()
-        outspecPaths = list()
-        # if(os.path.isdir(pklfile)):
-        #     #Look for all directories in specified path with structured folder name
-        #     fp1 = pklfile
-        #     dirs = [myString for myString in next(os.walk(fp1))[1] if 'SU' in myString \
-        #         and 'PP' in myString \
-        #         and 'OB' in myString \
-        #         and 'fZ' in myString \
-        #         and 'RS' in myString]  # Folders containing Monte Carlo Runs
-
-        #     for i in np.arange(len(dirs)):
-        #         pklFiles = [myFileName for myFileName in os.listdir(fp1+dirs[i]) if 'run' in myFileName and '.pkl' in myFileName]  # Get names of all pkl files in path
-        #         pklfname.append(np.random.choice(pklFiles))
-        #         pklPaths.append(fp1 + dirs[i] + '/' + pklfname[i])  # append a random pkl file to path
-        #         outspecPaths.append(fp1 + dirs[i] + '/' + 'outspec.json')
-        #elif(os.path.isfile(pklfile)):
         
         pkldir = [pklfile.split('/')[-2]]
         pklfname = pklfile.split('/')[-1].split('.')[0]
 
-            # pklPaths.append(pklfile)#append filepath provided in args
-            # outspecPaths.append(outspecfile)#append filepath provided in args
-
-        try:
-            with open(pklfile, 'rb') as f:#load from cache
-                DRM = pickle.load(f)
-        except:
-            vprint('Failed to open pklfile %s'%pklfile)
-            pass
-        try:
-            with open(outspecfile, 'rb') as g:
-                outspec = json.load(g)
-        except:
-            vprint('Failed to open outspecfile %s'%outspecfile)
-            pass
+        DRM, outspec = self.loadFiles(pklfile, outspecfile)
 
         arrival_times = [DRM['DRM'][i]['arrival_time'].value for i in np.arange(len(DRM['DRM']))]
         sumOHTIME = outspec['settlingTime'] + outspec['starlightSuppressionSystems'][0]['ohTime']
@@ -185,10 +156,7 @@ class DRMtoTimelinePlot(object):
             bl = patch.get_xy()
             x = 0.5*patch.get_width() + bl[0]
             y = 0.5*patch.get_height() + bl[1]
-            plt.rc('axes',linewidth=2)
-            plt.rc('lines',linewidth=2)
-            rcParams['axes.linewidth']=2
-            rc('font',weight='bold')
+            self.prettyPlot()
             if ObstextBool: 
                 ax.text(x, y, "Obs#%d, %dd" % (l,det_time), ha='center',va='center',rotation='vertical', fontsize=8)
 
@@ -210,50 +178,197 @@ class DRMtoTimelinePlot(object):
         # Plot Asthetics
         y_pos = np.arange(2)#Number of xticks to have
 
-        rc('axes',linewidth=2)
-        rc('lines',linewidth=2)
-        rcParams['axes.linewidth']=2
-        rc('font',weight='bold') 
+        self.prettyPlot()
         ax.set_yticks(y_pos)
         ax.set_yticklabels(('Obs','OB'),fontsize=12)
         ax.set_xlabel('Current Normalized Time (days)', weight='bold',fontsize=12)
         title('Mission Timeline for runName: ' + pkldir[0] + '\nand pkl file: ' + pklfname, weight='bold',fontsize=12)
         plt.tight_layout()
         plt.show(block=False)
-        savefig('/'.join(pklfile.split('/')[:-1]) + '/' + pkldir + 'Timeline' + '.png')
-        savefig('/'.join(pklfile.split('/')[:-1]) + '/' + pkldir + 'Timeline' + '.svg')
-        savefig('/'.join(pklfile.split('/')[:-1]) + '/' + pkldir + 'Timeline' + '.eps')
 
-        #plt.close()
+        date = unicode(datetime.datetime.now())
+        date = ''.join(c + '_' for c in re.split('-|:| ',date)[0:-1])#Removes seconds from date
+        fname = 'Timeline_' + folder.split('/')[-1] + '_' + date
+        plt.savefig(os.path.join(PPoutpath,fname+'.png'))
+        plt.savefig(os.path.join(PPoutpath,fname+'.svg'))
+        plt.savefig(os.path.join(PPoutpath,fname+'.eps'))
 
+    def prettyPlot(self):
+        """ Makes Plots Pretty
+        """
+        plt.rc('axes',linewidth=2)
+        plt.rc('lines',linewidth=2)
+        plt.rcParams['axes.linewidth']=2
+        plt.rc('font',weight='bold')
 
+    def loadFiles(self,pklfile,outspecfile):
+        """ loads pkl and outspec files
+        Args:
+            pklfile (string) - full filepath to pkl file to load
+            outspecfile (string) - fille filepath to outspec.json file
+        Return:
+            DRM (dict) - a dict containing seed, DRM, system
+            outspec (dict) - a dict containing input instructions
+        """
+        try:
+            with open(pklfile, 'rb') as f:#load from cache
+                DRM = pickle.load(f)
+        except:
+            print('Failed to open pklfile %s'%pklfile)
+            pass
+        try:
+            with open(outspecfile, 'rb') as g:
+                outspec = json.load(g)
+        except:
+            print('Failed to open outspecfile %s'%outspecfile)
+            pass
+        return DRM, outspec
 
+    def plotSnakingTimeline(self, pklfile='./', outspecfile='./', PPoutpath='./'):
+        """Plots a Timeline where each year is a "new line" on the chart
+        Args:
+            pklfile (string) - full filepath to pkl file to load
+            outspecfile (string) - fille filepath to outspec.json file
+            PPoutpath (string) - 
+        """
+        DRM, outspec = self.loadFiles(pklfile, outspecfile)
 
-        #Simple Plot of Temporal Coverage of Sky
-        tmpfig2 = plt.figure(figsize=(8,8),num=1)
-        rc('axes',linewidth=2)
-        rc('lines',linewidth=2)
-        rcParams['axes.linewidth']=2
-        rc('font',weight='bold')
-        sb1 = plt.subplot(111,projection="hammer")
-        plt.grid(True)
-        #sb1.scatter(np.pi/2,np.pi/4,color='r')#plot([0,np.pi/2,0],color='r')
-        #sb1.plot([np.pi/2,0,0],color='b')
-        OBdur = np.asarray(outspec['OBendTimes'])-np.asarray(outspec['OBstartTimes'])
-        angular_widths = np.asarray(OBdur)/365.25*360*np.pi/180.
-        #FOR OBSERVATIONS#start_angles = np.asarray(outspec['arrival_times'])%365.25 /365.25*360*np.pi/180.
-        start_angles = np.asarray(outspec['OBstartTimes'])%365.25 /365.25*360*np.pi/180. - np.pi
-        for i in range(len(start_angles)):
-            rect = Rectangle((start_angles[i],-np.pi/2),angular_widths[i],np.pi,angle=0.0, alpha=0.2)
-            sb1.add_patch(rect)
-        # rect = Rectangle((0,-np.pi/2),np.pi/4.,np.pi,angle=0.0, alpha=0.2)
-        # sb1.add_patch(rect)
-        title('Mission Length: ' + str(outspec['missionLife'])  +'yr OBduration: ' + str(outspec['OBduration']) + 'd MissionPortion: ' + str(outspec['missionPortion']),weight='bold',fontsize=12)
-        plt.show(block=False)
-        savefig('/'.join(pklfile.split('/')[:-1]) + '/' + pkldir + 'SkyCoverage' + '.png')
-        savefig('/'.join(pklfile.split('/')[:-1]) + '/' + pkldir + 'SkyCoverage' + '.svg')
-        savefig('/'.join(pklfile.split('/')[:-1]) + '/' + pkldir + 'SkyCoverage' + '.eps')
+        allModes = outspec['observingModes']
+        mode1 = [mode for mode in allModes if 'detectionMode' in mode.keys()]
+        assert len(mode1) == 1, 'This needs to be enhanced'
+        mode = mode1[0]
+        if not 'timeMultiplier' in mode.keys():
+            mode['timeMultiplier'] = 1.
 
-
+        LD = np.arange(len(DRM['DRM']))
+        arrival_times = [DRM['DRM'][i]['arrival_time'].value for i in LD]
+        sumOHTIME = outspec['settlingTime'] + outspec['starlightSuppressionSystems'][0]['ohTime']
+        det_times = [DRM['DRM'][i]['det_time'].value*(mode['timeMultiplier'])+sumOHTIME for i in LD]
+        det_timesROUNDED = [round(DRM['DRM'][i]['det_time'].value*(mode['timeMultiplier'])+sumOHTIME,1) for i in LD]
+        ObsNums = [DRM['DRM'][i]['ObsNum'] for i in LD]
+        y_vals = np.zeros(len(det_times)).tolist()
+        char_times = [DRM['DRM'][i]['char_time'].value*(1.+outspec['charMargin'])+sumOHTIME*(DRM['DRM'][i]['char_time'].value > 0.) for i in LD]
+        OBdurations = np.asarray(outspec['OBendTimes'])-np.asarray(outspec['OBstartTimes'])
         
+        #print(sum(det_times))
+        #print(sum(char_times))
+
+        from pylab import *
+        arr = [DRM['DRM'][i]['arrival_time'].value for i in np.arange(len(DRM['DRM']))]
+        dt = [DRM['DRM'][i]['det_time'].value + 1. for i in np.arange(len(DRM['DRM']))]
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        colors=['red','blue']
+        for i in np.arange(len(arr)):
+            ax.barh(1., dt[i], align='center', left=arr[i],color=colors[int(i) % len(colors)])
+        plt.show(block=False)
+
+
+        #Check if plotting font #########################################################
+        tmpfig = plt.figure(figsize=(20,5),num=0)
+        ax = tmpfig.add_subplot(111)
+        t = ax.text(0, 0, "Obs#   ,  d", ha='center',va='center',rotation='vertical', fontsize=8)
+        r = tmpfig.canvas.get_renderer()
+        bb = t.get_window_extent(renderer=r)
+        Obstxtwidth = bb.width#Width of text
+        Obstxtheight = bb.height#height of text
+        FIGwidth, FIGheight = tmpfig.get_size_inches()*tmpfig.dpi
+        #plt.show(block=False)
+        plt.close()
+        daysperpixelapprox = max(arrival_times)/FIGwidth#approximate #days per pixel
+        if mean(det_times)*0.8/daysperpixelapprox > Obstxtwidth:
+            ObstextBool = True
+        else:
+            ObstextBool = False
+
+        tmpfig = plt.figure(figsize=(25,5),num=0)
+        ax = tmpfig.add_subplot(111)
+        t = ax.text(0, 0, "OB#  , dur.=    d", ha='center',va='center',rotation='horizontal', fontsize=12)
+        r = tmpfig.canvas.get_renderer()
+        bb = t.get_window_extent(renderer=r)
+        OBtxtwidth = bb.width#Width of text
+        OBtxtheight = bb.height#height of text
+        FIGwidth, FIGheight = tmpfig.get_size_inches()*tmpfig.dpi
+        #plt.show(block=False)
+        plt.close()
+        if mean(OBdurations)*0.8/daysperpixelapprox > OBtxtwidth:
+            OBtextBool = True
+        else:
+            OBtextBool = False
+        #################################################################################
+
+
+        ######################################################################
+        #Finds arrival times that occur within that year
+        ObsNumsL = list()
+        det_timesL = list()
+        char_timesL = list()
+        arrival_timesL = list()
+        truthArr = list()
+        for i in np.arange(int(np.ceil(max(arrival_times)/365.25))):
+            truthArr = (np.asarray(arrival_times) >= 365.25*np.float(i))*(np.asarray(arrival_times) < 365.25*np.float(i+1.))
+            arrival_timesL.append([arrival_times[ii] for ii in np.where(truthArr)[0]])
+            det_timesL.append([det_times[ii] for ii in np.where(truthArr)[0]])
+            char_timesL.append([char_times[ii] for ii in np.where(truthArr)[0]])
+            ObsNumsL.append([ObsNums[ii] for ii in np.where(truthArr)[0]])
+        #######################################################################
+
+        #######################################################################
+        # Plotting 
+        colors = 'rb'#'rgbwmc'
+        patch_handles = []
+        fig = plt.figure(figsize=(20,3+int(np.ceil(max(arrival_times)/365.25))/2.),num=cnt)
+        self.prettyPlot()
+        ax = fig.add_subplot(111)
+
+        char_color=(0./255.,128/255.,0/255.)
+
+        #Plot individual blocks
+        # Plot All Detection Observations for Year
+        for iyr in np.arange(int(np.ceil(max(arrival_times)/365.25))):
+            ind = 0
+            obs = 0
+            for (det_time, l, char_time, arrival_times_yr) in zip(det_timesL[iyr], ObsNumsL[iyr], char_timesL[iyr], arrival_timesL[iyr]):
+                #print det_time, l
+                patch_handles.append(ax.barh(int(np.ceil(max(arrival_times)/365.25))-iyr, det_time, align='center', left=arrival_times_yr-365.25*iyr,
+                    color=colors[int(obs) % len(colors)]))
+                if not char_time == 0.:
+                    ax.barh(int(np.ceil(max(arrival_times)/365.25))-iyr, char_time, align='center', left=arrival_times_yr+det_time-365.25*iyr,color=char_color)
+                ind += 1
+                obs += 1
+                patch = patch_handles[-1][0]
+                bl = patch.get_xy()
+                x = 0.5*patch.get_width() + bl[0]
+                y = 0.5*patch.get_height() + bl[1]
+                if ObstextBool: 
+                    ax.text(x, y, "Obs#%d, %dd" % (l,det_time), ha='center',va='center',rotation='vertical', fontsize=8)
+
+        #Set Plot Xlimit so the end of the timeline is at the end of the figure box
+        ax.set_xlim([None, 365.25])
+
+
+        # Plot Asthetics
+        y_pos = np.arange(int(np.ceil(max(arrival_times)/365.25)))+1#Number of xticks to have
+
+        yticklabels = list()
+        for i in np.arange(int(np.ceil(max(arrival_times)/365.25))):
+            yticklabels.append(str(int(np.ceil(max(arrival_times)/365.25)) - i) + 'yr')
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(yticklabels,fontsize=30)
+        #ax.set_yticklabels(('3yr','2yr','1yr'),fontsize=30)
+        ax.xaxis.set_tick_params(labelsize=30)
+        ax.set_xlabel('Time Since Start of Mission Year (days)', weight='bold',fontsize=30)
+        plt.title('Mission Timeline for runName: ' + dirs[cnt] + '\nand pkl file: ' + pklfname[cnt], weight='bold',fontsize=12)
+        plt.tight_layout()
+        plt.show(block=False)
+
+        date = unicode(datetime.datetime.now())
+        date = ''.join(c + '_' for c in re.split('-|:| ',date)[0:-1])#Removes seconds from date
+        fname = 'TimelineSnake_' + folder.split('/')[-1] + '_' + date
+        plt.savefig(os.path.join(PPoutpath,fname+'.png'))
+        plt.savefig(os.path.join(PPoutpath,fname+'.svg'))
+        plt.savefig(os.path.join(PPoutpath,fname+'.eps'))
+
+
 
