7c1ce3e3a22593c50c9de850c58d55007f37395e
Dean Keithly <drk94@cornell.edu>
2019-04-08 14:24:01 -0400

    deleting useless file

--- tmp.py
+++
@@ -1,5 +0,0 @@
-from EXOSIMS.util.plotC0vsT0andCvsT import plotC0vsT0andCvsT as PTT
-folder = '/home/dean/Documents/SIOSlab/EXOSIMSres/Dean2June18RS09CXXfZ01OB64PP01SU01'
-PPoutpath = '/home/dean/Documents/SIOSlab/EXOSIMSres/Dean2June18RS09CXXfZ01OB64PP01SU01'
-ptt = PTT()
-ptt.singleRunPostProcessing(PPoutpath,folder)
