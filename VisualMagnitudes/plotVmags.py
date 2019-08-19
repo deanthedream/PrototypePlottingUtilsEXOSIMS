63a2a9385953eb598952b68ecc959e90f13d1b77
Dean Keithly <drk94@cornell.edu>
2019-08-19 16:41:20 -0600

    adding star visual magnitudes and some WFIRST_DIAGRAM plots

---
+++ VisualMagnitudes/plotVmags.py
@@ -0,0 +1,42 @@
+## Plots Visual Magnitudes of Objects
+
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as mdates
+
+
+# Choose some nice levels
+levels = np.tile([-5, 5, -3, 3, -1, 1],
+                 int(np.ceil(len(dates)/6)))[:len(dates)]
+
+# Create figure and plot a stem plot with the date
+fig, ax = plt.subplots(figsize=(8.8, 4), constrained_layout=True)
+ax.set(title="Astronomical Visual Magnitudes")
+
+markerline, stemline, baseline = ax.stem(dates, levels,
+                                         linefmt="C3-", basefmt="k-",
+                                         use_line_collection=True)
+
+plt.setp(markerline, mec="k", mfc="w", zorder=3)
+
+# Shift the markers to the baseline by replacing the y-data by zeros.
+markerline.set_ydata(np.zeros(len(dates)))
+
+# annotate lines
+vert = np.array(['top', 'bottom'])[(levels > 0).astype(int)]
+for d, l, r, va in zip(dates, levels, names, vert):
+    ax.annotate(r, xy=(d, l), xytext=(-3, np.sign(l)*3),
+                textcoords="offset points", va=va, ha="right")
+
+# format xaxis with 4 month intervals
+ax.get_xaxis().set_major_locator(mdates.MonthLocator(interval=4))
+ax.get_xaxis().set_major_formatter(mdates.DateFormatter("%b %Y"))
+plt.setp(ax.get_xticklabels(), rotation=30, ha="right")
+
+# remove y axis and spines
+ax.get_yaxis().set_visible(False)
+for spine in ["left", "top", "right"]:
+    ax.spines[spine].set_visible(False)
+
+ax.margins(y=0.1)
+plt.show()
