2bf33f4a6212be9b3c95e1c44af60f792e73ede3
Dean Keithly <drk94@cornell.edu>
2019-12-22 22:41:51 -0500

    substantial changes to sub-type completeness plots

--- plotSubTypeComp.py
+++ plotSubTypeComp.py
@@ -29,7 +29,13 @@ comp = sim.Completeness
 #### Plot Population JPDF
 plt.close(983098098203845)
 plt.figure(num=983098098203845)
-plt.contourf(comp.xnew,comp.ynew,comp.Cpdf_pop,cmap='jet', locator=ticker.LogLocator())
+plt.contourf(comp.xnew,comp.ynew,comp.Cpdf_pop,cmap='jet', locator=ticker.LogLocator(), zorder=0)
+for ii,j in itertools.product(np.arange(len(comp.Rp_hi)),np.arange(len(comp.L_lo[0,:]))):
+    for k in np.arange(len(comp.jpdf_props['lower_limits'][ii,j])):
+        start = comp.jpdf_props['lower_limits'][ii,j][k]
+        stop = comp.jpdf_props['upper_limits'][ii,j][k]
+        sranges = np.linspace(start=start,stop=stop,num=200)
+        plt.plot(sranges,comp.jpdf_props['limit_funcs'][ii,j][k](sranges),color='black', zorder=10)
 plt.show(block=False)
 
 #### Plot EarthLike JPDF
@@ -55,6 +61,139 @@ def plotIndividualSubTypeJPDFs(comp):
 
 
 #### Plot Gridspec of Kopparapu Bins 1111111111111111111111111111111111111
+plt.close(9876)
+fig9876 = plt.figure(num=9876, figsize=(len(comp.L_lo[0,:]-2)*4,len(comp.Rp_bins-1)*4+0.75))
+numRows = len(comp.Rp_bins)-1-1+1 #Rp bins + 1 colorbar
+numCols = len(comp.L_lo[0,:])-2 #Luminosity bins
+#height_ratios = ([0.75] + [3,0.5]*(len(comp.Rp_bins)-1))[:-1]
+#width_ratios = ([3,0.5]*(len(comp.L_lo)))[:-1]
+height_ratios = ([0.75] + [4]*(len(comp.Rp_bins)-1-1))
+width_ratios = ([4]*(len(comp.L_lo[0,:])-2))
+gs = gridspec.GridSpec(numRows,numCols, width_ratios=width_ratios, height_ratios=height_ratios)
+gs.update(wspace=0.03, hspace=0.05) # set the spacing between axes. 
+plt.rc('axes',linewidth=2)
+plt.rc('lines',linewidth=2)
+plt.rcParams['axes.linewidth']=2
+plt.rc('font',weight='bold')
+
+#Find Levels for Contour Colors
+tmpH = comp.Cpdf_pop.copy()
+tmpH[tmpH==0.] = np.nan
+cscaleMin = np.floor(np.nanmin(np.log10(tmpH))) # 10**min, min order of magnitude
+cscaleMax = np.ceil(np.nanmax(np.log10(tmpH))) # 10**max, max order of magnitude
+levels = 10.**np.arange(cscaleMin,cscaleMax+1)
+
+#Find xmin, xmax, ymin, ymax
+xmin=0.
+xmax=25.
+ymin=15.
+ymax=50.
+
+#What the plot layout looks like
+###---------------------------------------------------------
+# | gs[0]  gs[1]  gs[2]  |
+# | gs[3]  gs[4]  gs[5]  |
+# | gs[6]  gs[7]  gs[8]  |
+# | gs[9]  gs[10] gs[11] |
+# | gs[12] gs[13] gs[14] |
+# | gs[15] gs[16] gs[17] |
+# | gs[18] gs[19] gs[20] |
+# | gs[21] gs[22] gs[23] |
+###---------------------------------------------------------
+axCBAR = plt.subplot(gs[0:2])
+axij = dict()
+for ii,j in itertools.product(np.arange(len(comp.Rp_hi)-1)+1,np.arange(len(comp.L_lo[0,:])-2)+1):
+    #DELETE axij[ii,j] = plt.subplot(gs[5+j+ii*len(comp.L_lo[0,:])]) #old mapping
+    axij[ii,j] = plt.subplot(gs[2+j+(len(comp.Rp_hi)-1-ii)*(len(comp.L_lo[0,:])-2)])#3 from cbar, j iterates over row rest defines starting spot
+    axij[ii,j].contourf(comp.xnew,comp.ynew,comp.Cpdf_hs[ii,j],cmap='jet', levels=levels, norm = LogNorm())
+    axij[ii,j].set_xlim([xmin,xmax])
+    axij[ii,j].set_ylim([ymin,ymax])
+    axij[ii,j].text(18.5,45,'(i:' + str(ii) + ',j:' + str(j) + ')', weight='bold')
+    if j != 1:
+        axij[ii,j].get_yaxis().set_visible(False)
+    if ii != 1:#len(comp.Rp_hi)-1:
+        axij[ii,j].get_xaxis().set_visible(False)
+    axij[ii,j].text(0.5,45, comp.type_names[ii,j], weight='bold')
+    #Add total Count per grid
+    axij[ii,j].text(16,41, "{:.2e}".format(comp.count_hs[ii,j]),weight='bold')
+    #Add bounding edges
+    for k in np.arange(len(comp.jpdf_props['lower_limits'][ii,j])):
+        start = comp.jpdf_props['lower_limits'][ii,j][k]
+        stop = comp.jpdf_props['upper_limits'][ii,j][k]
+        sranges = np.linspace(start=start,stop=stop,num=200)
+        axij[ii,j].plot(sranges,comp.jpdf_props['limit_funcs'][ii,j][k](sranges),color='black')
+    #Add bounding edges For Other in Column
+    for ii2 in (np.arange(len(comp.Rp_hi)-ii-1-1)+ii+1+1): #All plots above this one in the column
+        for k in np.arange(len(comp.jpdf_props['lower_limits'][ii2,j])-1):
+            start = comp.jpdf_props['lower_limits'][ii2,j][k]
+            stop = comp.jpdf_props['upper_limits'][ii2,j][k]
+            sranges = np.linspace(start=start,stop=stop,num=200)
+            axij[ii,j].plot(sranges,comp.jpdf_props['limit_funcs'][ii2,j][k](sranges),color='red',linewidth=1.0)
+    #Add dmag and s, (dmag=23, s=0.7) - Earth from dmag vs s plot of solar system
+    earth_separation = 0.7 #AU
+    earth_dmag = 23. #planet-star difference in magnitude
+    axij[ii,j].scatter(earth_separation,earth_dmag,c='white',s=2,edgecolor='black',linewidth=0.5)
+    #Add ddmag and ds Error Bars
+    uncertainty_dmag = 0.01 #HabEx requirement is 1%
+    uncertainty_s = 5.*u.mas.to('rad')*10.*u.pc.to('AU')
+    axij[ii,j].errorbar(earth_separation, earth_dmag, yerr=[uncertainty_dmag*earth_dmag,], xerr=[uncertainty_s], fmt='--',color='black',linewidth=1)
+fig9876.tight_layout(pad=0)
+
+#Temporary For coloring purposes, pop hist
+plt.close(9000)
+figpop = plt.figure(num=9000)
+ax1= plt.gca()
+cax = ax1.contourf(comp.xnew, comp.ynew, comp.Cpdf_pop, extent=[xmin, xmax, ymin, ymax], cmap='jet', levels=levels, norm = LogNorm())
+CS4 = ax1.contour(cax, colors=('k',), linewidths=(1,), origin='lower', levels=levels, norm = LogNorm())
+for k in np.arange(len(comp.lower_limits)):
+    start = comp.lower_limits[k]
+    stop = comp.upper_limits[k]
+    sranges = np.linspace(start=start,stop=stop,num=200)
+    ax1.plot(sranges, comp.dmag_limit_functions[k](sranges),color='black')
+for ii,j in itertools.product(np.arange(len(comp.Rp_hi)-1)+1,np.arange(len(comp.L_lo[0,:])-2)+1):
+    for k in np.arange(len(comp.jpdf_props['lower_limits'][ii,j])):
+        start = comp.jpdf_props['lower_limits'][ii,j][k]
+        stop = comp.jpdf_props['upper_limits'][ii,j][k]
+        sranges = np.linspace(start=start,stop=stop,num=200)
+        ax1.plot(sranges,comp.jpdf_props['limit_funcs'][ii,j][k](sranges),color='black', zorder=10)
+ax1.set_ylim([ymin,ymax])
+ax1.set_xlim([xmin,xmax])
+
+#Add Colorbar, and pop dist
+cbar = fig9876.colorbar(cax, cax=axCBAR, orientation='horizontal')#pad=0.05,
+plt.rcParams['axes.titlepad']=-10
+axCBAR.set_xlabel('Joint Probability Density: Planet Sub-types', weight='bold', labelpad=-55)
+axCBAR.tick_params(axis='x',direction='in',labeltop=True,labelbottom=False)#'off'
+cbar.add_lines(CS4)
+plt.title('SAG13 Population JPDF', weight='bold')
+plt.xlabel('Luminosity Scaled Planet-star Separation ' + r'$(s/\sqrt{L})$' + ' in AU', weight='bold')
+plt.ylabel('Planet-star Difference in Magnitude', weight='bold')
+plt.text(20,48,"{:.2e}".format(comp.count_pop),weight='bold')
+
+#Add Labels
+fig9876.text(0.5, 0.075, 'Luminosity Scaled Planet-star Separation ' + r'$(s/\sqrt{L})$' + ' in AU', ha='center', weight='bold')
+fig9876.text(0.09, 0.5, 'Planet-star Difference in Magnitude', va='center', rotation='vertical', weight='bold')
+
+#### add Earth-Like dist
+plt.close(2)
+figearth = plt.figure(num=2)
+ax2= plt.gca()
+cax2 = ax2.contourf(comp.xnew, comp.ynew, comp.Cpdf_earthLike, extent=[xmin, xmax, ymin, ymax], cmap='jet', levels=levels, norm = LogNorm())
+CS42 = ax2.contour(cax2, colors=('k',), linewidths=(1,), origin='lower', levels=levels, norm = LogNorm())
+ax2.set_ylim([ymin,ymax])
+ax2.set_xlim([xmin,xmax])
+plt.text(20,48,"{:.2e}".format(comp.count_earthLike),weight='bold')
+plt.title('SAG13 Earth-Like Sub-Population JPDF', weight='bold')
+plt.xlabel('Luminosity Scaled Planet-star Separation ' + r'$(s/\sqrt{L})$' + ' in AU', weight='bold')
+plt.ylabel('Planet-star Difference in Magnitude', weight='bold')
+
+plt.show(block=False)
+print(saltyburrito)
+###########################################################################
+
+
+
+#### Plot Gridspec of Kopparapu Bins 1111111111111111111111111111111111111
 plt.close(1)
 fig = plt.figure(num=1, figsize=(len(comp.L_lo[0,:])*3,len(comp.Rp_bins)*3+0.75))
 numRows = len(comp.Rp_bins)-1+1 #Rp bins + 1 colorbar
@@ -64,7 +203,7 @@ numCols = len(comp.L_lo[0,:]) #Luminosity bins
 height_ratios = ([0.75] + [3]*(len(comp.Rp_bins)-1))
 width_ratios = ([3]*(len(comp.L_lo[0,:])))
 gs = gridspec.GridSpec(numRows,numCols, width_ratios=width_ratios, height_ratios=height_ratios)
-gs.update(wspace=0.06, hspace=0.06) # set the spacing between axes. 
+gs.update(wspace=0.03, hspace=0.05) # set the spacing between axes. 
 plt.rc('axes',linewidth=2)
 plt.rc('lines',linewidth=2)
 plt.rcParams['axes.linewidth']=2
@@ -111,17 +250,27 @@ for ii,j in itertools.product(np.arange(len(comp.Rp_hi)),np.arange(len(comp.L_lo
     #Add total Count per grid
     axij[ii,j].text(16,41, "{:.2e}".format(comp.count_hs[ii,j]),weight='bold')
     #Add bounding edges
-    #sranges = list()
     for k in np.arange(len(comp.jpdf_props['lower_limits'][ii,j])):
         start = comp.jpdf_props['lower_limits'][ii,j][k]
         stop = comp.jpdf_props['upper_limits'][ii,j][k]
-        sranges = np.linspace(start=start,stop=stop,num=100)
-        # if len(sranges[0]) == 5:
-        #     print(saltyburrito)
-
+        sranges = np.linspace(start=start,stop=stop,num=200)
         axij[ii,j].plot(sranges,comp.jpdf_props['limit_funcs'][ii,j][k](sranges),color='black')
-    #axij[ii,j].plot(sranges[1],comp.jpdf_props['limit_funcs'][ii,j][1](sranges[1]*u.AU),color='black')
-    #axij[ii,j].plot(sranges[2],comp.jpdf_props['limit_funcs'][ii,j][2](sranges[2]*u.AU),color='black')
+    #Add bounding edges For Other in Column
+    for ii2 in (np.arange(len(comp.Rp_hi)-ii-1)+ii+1): #All plots above this one in the column
+        for k in np.arange(len(comp.jpdf_props['lower_limits'][ii2,j])-1):
+            start = comp.jpdf_props['lower_limits'][ii2,j][k]
+            stop = comp.jpdf_props['upper_limits'][ii2,j][k]
+            sranges = np.linspace(start=start,stop=stop,num=200)
+            axij[ii,j].plot(sranges,comp.jpdf_props['limit_funcs'][ii2,j][k](sranges),color='red',linewidth=1.0)
+    #Add dmag and s, (dmag=23, s=0.7) - Earth from dmag vs s plot of solar system
+    earth_separation = 0.7 #AU
+    earth_dmag = 23. #planet-star difference in magnitude
+    axij[ii,j].scatter(earth_separation,earth_dmag,c='white',s=2,edgecolor='black',linewidth=0.5)
+    #Add ddmag and ds Error Bars
+    uncertainty_dmag = 0.01 #HabEx requirement is 1%
+    uncertainty_s = 5.*u.mas.to('rad')*10.*u.pc.to('AU')
+    axij[ii,j].errorbar(earth_separation, earth_dmag, yerr=[uncertainty_dmag*earth_dmag,], xerr=[uncertainty_s], fmt='--',color='black',linewidth=1)
+fig.tight_layout(pad=0)
 
 #Temporary For coloring purposes, pop hist
 plt.close(9000)
@@ -129,6 +278,17 @@ figpop = plt.figure(num=9000)
 ax1= plt.gca()
 cax = ax1.contourf(comp.xnew, comp.ynew, comp.Cpdf_pop, extent=[xmin, xmax, ymin, ymax], cmap='jet', levels=levels, norm = LogNorm())
 CS4 = ax1.contour(cax, colors=('k',), linewidths=(1,), origin='lower', levels=levels, norm = LogNorm())
+for k in np.arange(len(comp.lower_limits)):
+    start = comp.lower_limits[k]
+    stop = comp.upper_limits[k]
+    sranges = np.linspace(start=start,stop=stop,num=200)
+    ax1.plot(sranges, comp.dmag_limit_functions[k](sranges),color='black')
+for ii,j in itertools.product(np.arange(len(comp.Rp_hi)),np.arange(len(comp.L_lo[0,:]))):
+    for k in np.arange(len(comp.jpdf_props['lower_limits'][ii,j])):
+        start = comp.jpdf_props['lower_limits'][ii,j][k]
+        stop = comp.jpdf_props['upper_limits'][ii,j][k]
+        sranges = np.linspace(start=start,stop=stop,num=200)
+        ax1.plot(sranges,comp.jpdf_props['limit_funcs'][ii,j][k](sranges),color='black', zorder=10)
 ax1.set_ylim([ymin,ymax])
 ax1.set_xlim([xmin,xmax])
 
@@ -139,12 +299,12 @@ axCBAR.set_xlabel('Joint Probability Density: Planet Sub-types', weight='bold', 
 axCBAR.tick_params(axis='x',direction='in',labeltop=True,labelbottom=False)#'off'
 cbar.add_lines(CS4)
 plt.title('SAG13 Population JPDF', weight='bold')
-plt.xlabel('Planet-star Separation ' + r'$(s)$' + ' in AU', weight='bold')
+plt.xlabel('Luminosity Scaled Planet-star Separation ' + r'$(s/\sqrt{L})$' + ' in AU', weight='bold')
 plt.ylabel('Planet-star Difference in Magnitude', weight='bold')
 plt.text(20,48,"{:.2e}".format(comp.count_pop),weight='bold')
 
 #Add Labels
-fig.text(0.5, 0.075, 'Planet-star Separation ' + r'$(s)$' + ' in AU', ha='center', weight='bold')
+fig.text(0.5, 0.075, 'Luminosity Scaled Planet-star Separation ' + r'$(s/\sqrt{L})$' + ' in AU', ha='center', weight='bold')
 fig.text(0.09, 0.5, 'Planet-star Difference in Magnitude', va='center', rotation='vertical', weight='bold')
 
 #### add Earth-Like dist
@@ -157,7 +317,7 @@ ax2.set_ylim([ymin,ymax])
 ax2.set_xlim([xmin,xmax])
 plt.text(20,48,"{:.2e}".format(comp.count_earthLike),weight='bold')
 plt.title('SAG13 Earth-Like Sub-Population JPDF', weight='bold')
-plt.xlabel('Planet-star Separation ' + r'$(s)$' + ' in AU', weight='bold')
+plt.xlabel('Luminosity Scaled Planet-star Separation ' + r'$(s/\sqrt{L})$' + ' in AU', weight='bold')
 plt.ylabel('Planet-star Difference in Magnitude', weight='bold')
 
 plt.show(block=False)
@@ -267,12 +427,12 @@ axCBAR.set_xlabel('Joint Probability Density: Planet Sub-types', weight='bold', 
 axCBAR.tick_params(axis='x',direction='in',labeltop=True,labelbottom=False)#'off'
 cbar.add_lines(CS4)
 plt.title('SAG13 Population JPDF', weight='bold')
-plt.xlabel('Planet-star Separation ' + r'$(s)$' + ' in AU', weight='bold')
+plt.xlabel('Luminosity Scaled Planet-star Separation ' + r'$(s/\sqrt{L})$' + ' in AU', weight='bold')
 plt.ylabel('Planet-star Difference in Magnitude', weight='bold')
 plt.text(20,48,"{:.2e}".format(comp.count_pop),weight='bold')
 
 #Add Labels
-fig2.text(0.5, 0.075, 'Planet-star Separation ' + r'$(s)$' + ' in AU', ha='center', weight='bold')
+fig2.text(0.5, 0.075, 'Luminosity Scaled Planet-star Separation ' + r'$(s/\sqrt{L})$' + ' in AU', ha='center', weight='bold')
 fig2.text(0.09, 0.5, 'Planet-star Difference in Magnitude', va='center', rotation='vertical', weight='bold')
 
 plt.show(block=False)
