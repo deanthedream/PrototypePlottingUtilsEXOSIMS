d59c074b2bae13cd379de465ca5d7adfb88809e4
Dean Keithly <drk94@cornell.edu>
2020-09-28 09:54:35 -0400

    adding timing calculations and printouts

--- SolarSystemdMag/numericalNuFromDmag.py
+++ SolarSystemdMag/numericalNuFromDmag.py
@@ -16,6 +16,7 @@ try:
 except:
     import pickle
 from EXOSIMS.util.deltaMag import deltaMag
+import time
 
 #### PLOT BOOL
 plotBool = True
@@ -48,12 +49,14 @@ sma, e, p, Rp = PPop.gen_plan_params(n)
 sma = sma.to('AU').value
 
 a = sma*u.AU
-dmag = 29. #specify the dmag to calculate for
+dmag = 25. #specify the dmag to calculate for
 
 #This is a solver for nu from dmag assuming a quasi-lambert phase function fullEqnX from AnalyticalNuFromDmag3.ipynb
 #Generate terms
 lhs = 10.**(-0.4*dmag)*(1.-e**2.)**2.*(a.to('AU')/Rp.to('AU')).decompose().value**2/p
 
+
+tstart_cos = time.time()
 #These are the coefficients starting at cos(nu)^8 
 A = e**4.*np.sin(inc)**4.*np.sin(w)**4./16. + e**4.*np.sin(inc)**4.*np.sin(w)**2.*np.cos(w)**2./8. + e**4.*np.sin(inc)**4.*np.cos(w)**4./16.
 B = e**4.*np.sin(inc)**3.*np.sin(w)**3./4. + e**4.*np.sin(inc)**3.*np.sin(w)*np.cos(w)**2./4. + e**3.*np.sin(inc)**4.*np.sin(w)**4./4. + e**3.*np.sin(inc)**4.*np.sin(w)**2.*np.cos(w)**2./2. + e**3.*np.sin(inc)**4.*np.cos(w)**4./4.
@@ -114,6 +117,7 @@ sumNumSol = np.sum(np.logical_xor(correctValBoolean1,bothBools)) + np.sum(np.log
 numSols = np.sum(np.logical_xor(correctValBoolean1,bothBools) + np.logical_xor(correctValBoolean2,bothBools) + bothBools,axis=1)
 numSolHist = np.histogram(numSols,bins=[-0.1,0.9,1.9,2.9,3.9,4.9])
 np.sum(np.histogram(numSols,bins=[-0.1,0.9,1.9,2.9,3.9,4.9])[0])
+tstop_cos = time.time()
 
 plt.figure(num=9000)
 #plt.hist(gdmags.flatten(),alpha=0.3,color='blue',bins=50)
@@ -122,6 +126,13 @@ plt.hist(gdmagsCombined.flatten(),alpha=0.3,color='red',bins=50)
 plt.yscale('log')
 plt.show(block=False)
 ####################################################################
+
+
+####Timing test
+ttstart = time.time()
+np.roots(coeffs[:,i])
+ttstop = time.time()
+
 
 ####################################################################
 # #Using the derivative dFullEqnX from AnalyticalNuFromDmag3.ipynb
@@ -196,8 +207,7 @@ def sinBhaskara_pi_2pi(x):
     """
     return (32.*np.pi**2. - 48.*np.pi*x + 16.*x**2.)/(13.*np.pi**2. - 12.*np.pi*x + 4.*x**2.)
 
-
-# Fitting a polynomial to 
+# Fitting a polynomial to ##C6 was removed for consistency with ipynb
 trigFuncs = [lambda x: np.sin(x)**2*np.cos(x)**2, #c0
 lambda x: np.sin(x)*np.cos(x)**3, #c1
 lambda x: np.cos(x)**4, #c2
@@ -210,17 +220,70 @@ lambda x: np.cos(x)**2, #C9
 lambda x: np.sin(x), #c10
 lambda x: np.cos(x)] #c11
 
-diffTrigFuncs = [lambda x: -2.*np.sin(x)**3.*np.cos(x) + 2.*np.sin(x)*np.cos(x)**3.,
-lambda x: -3.*np.sin(x)**2.*np.cos(x)**2. + np.cos(x)**4.,
-lambda x: -4.*np.sin(x)*np.cos(x)**3.,
-lambda x: -2.*np.sin(x)**2.*np.cos(x) + np.cos(x)**3.,
-lambda x: -3.*np.sin(x)*np.cos(x)**2.,
-lambda x: -np.sin(x)**3. + 2.*np.sin(x)*np.cos(x)**2.,
-lambda x: -np.sin(x)**2 + np.cos(x)**2,
-lambda x: 2*np.sin(x)*np.cos(x),
-lambda x: -2*np.sin(x)*np.cos(x),
-lambda x: np.cos(x),
-lambda x: -np.sin(x)]
+diffTrigFuncs = [lambda x: -2.*np.sin(x)**3.*np.cos(x) + 2.*np.sin(x)*np.cos(x)**3., #0 #OK
+lambda x: -3.*np.sin(x)**2.*np.cos(x)**2. + np.cos(x)**4., #1
+lambda x: -4.*np.sin(x)*np.cos(x)**3., #2
+lambda x: -2.*np.sin(x)**2.*np.cos(x) + np.cos(x)**3., #3
+lambda x: -3.*np.sin(x)*np.cos(x)**2., #4
+lambda x: -np.sin(x)**3. + 2.*np.sin(x)*np.cos(x)**2., #5 #OK
+lambda x: -np.sin(x)**2 + np.cos(x)**2, #7 #OK
+lambda x: 2.*np.sin(x)*np.cos(x), #8 OK 
+lambda x: -2.*np.sin(x)*np.cos(x), #9 #OK 
+lambda x: np.cos(x), #10 #OK
+lambda x: -np.sin(x)] #11 #OK
+
+#### Nice nu ranges for each function KEEP until done
+# xtmp = np.linspace(start=0, stop=2.*np.pi, num=200)
+# ytmps = list()
+# dytmps = list()
+# for i in np.arange(len(trigFuncs)):
+#     ytmp = trigFuncs[i](xtmp)
+#     ytmps.append(ytmp)
+#     plt.figure(num=i)
+#     plt.subplot(211)
+#     plt.plot(xtmp,ytmp,color='blue')
+#     plt.xlim([0.,2.*np.pi])
+#     plt.subplot(212)
+#     dytmp = diffTrigFuncs[i](xtmp)
+#     dytmps.append(dytmp)
+#     plt.plot(xtmp,dytmp,color='blue')
+#     plt.plot([0.,2.*np.pi],[0.,0.],color='black')
+#     plt.xlim([0.,2.*np.pi])
+#     plt.show(block=False)
+
+
+#The zeros to the diffTrigFuncs expressions
+zeros = [[0., -3.*np.pi/4., -np.pi/2., -np.pi/4., np.pi/4., np.pi/2., 3.*np.pi/4.],
+ [-5.*np.pi/6., -np.pi/2., -np.pi/6., np.pi/6., np.pi/2., 5.*np.pi/6.],
+ [0., np.pi/2., np.pi, 3.*np.pi/2.],
+ [-np.pi/2.,  np.pi/2.,  -2.*np.arctan(np.sqrt(5. - 2.*np.sqrt(6.))),  2.*np.arctan(np.sqrt(5. - 2.*np.sqrt(6.))),  -2.*np.arctan(np.sqrt(2.*np.sqrt(6.) + 5.)),  2.*np.arctan(np.sqrt(2.*np.sqrt(6.) + 5.))],
+ [0., np.pi/2., np.pi, 3.*np.pi/2.],
+ [0.,  -2.*np.arctan(np.sqrt(2. - np.sqrt(3.))),  2.*np.arctan(np.sqrt(2. - np.sqrt(3.))),  -2.*np.arctan(np.sqrt(np.sqrt(3.) + 2.)),  2.*np.arctan(np.sqrt(np.sqrt(3.) + 2.))],
+ [-np.pi/2.,  np.pi/2.,  -2.*np.arctan(np.sqrt(5. - 2.*np.sqrt(6.))),  2.*np.arctan(np.sqrt(5. - 2.*np.sqrt(6.))),  -2.*np.arctan(np.sqrt(2.*np.sqrt(6.) + 5.)),  2.*np.arctan(np.sqrt(2.*np.sqrt(6.) + 5.))],
+ [-np.pi/4., np.pi/4.],
+ [0., np.pi/2., np.pi, 3.*np.pi/2.],
+ [0., np.pi/2., np.pi, 3.*np.pi/2.],
+ [np.pi/2., 3.*np.pi/2.],
+ [0., np.pi]]
+zeros = [np.concatenate((np.asarray(zeros[i]),np.asarray(zeros[i])+np.pi)).tolist() for i in np.arange(len(zeros))]
+start_stop_points = zeros
+
+#Here we produce flattenedZeros2 which contains a set of unique angles in radians where the trigFuncs have min/max/saddle points
+flattenedZeros = list()
+for i in np.arange(len(zeros)):
+    flattenedZeros = flattenedZeros + zeros[i]
+flattenedZeros = np.asarray(flattenedZeros)
+flattenedZeros[flattenedZeros < 0] = flattenedZeros[flattenedZeros < 0] + 2.*np.pi
+flattenedZeros = np.unique(flattenedZeros)
+flattenedZeros2 = list()
+for i in np.arange(len(flattenedZeros)-1):
+    if np.abs(flattenedZeros[i]-flattenedZeros[i+1]) > 1e-10:
+        flattenedZeros2.append(flattenedZeros[i])
+flattenedZeros2 = np.asarray(flattenedZeros2) #works okay. A few bits with bad gaps
+tmp = flattenedZeros2[:-1][np.diff(flattenedZeros2)/2. > 0.1] + (np.diff(flattenedZeros2)/2.)[np.diff(flattenedZeros2)/2. > 0.1]
+flattenedZeros3 = np.sort(np.concatenate((flattenedZeros2,tmp)))
+####
+
 
 def polyFunc(x,nus):
     return x[0]*nus**4.+x[1]*nus**3.+x[2]*nus**2.+x[3]*nus+ x[4]
@@ -238,8 +301,11 @@ def polyCoeffsError(x,trigFunc,nus):
 cachedir = './'
 dfilename = 'dmagTrigParams' + '.pkl'
 path = os.path.join(cachedir, dfilename)
-start_stop_points = np.pi/8.*np.arange(int((2.*np.pi)/(np.pi/8.)))
-nuss = np.asarray([np.linspace(start=start_stop_points[i],stop=start_stop_points[i+1],num=50,endpoint=True) for i in np.arange(len(start_stop_points)-1)])
+#OLDOLD start_stop_points = np.pi/8.*np.arange(int((2.*np.pi)/(np.pi/8.))) #older set of points to fit
+#OLDstart_stop_points = np.pi/180.*360./15.*np.arange(int(360./15.))
+start_stop_points = flattenedZeros3
+nuss = np.asarray([np.linspace(start=start_stop_points[i],stop=start_stop_points[i+1],num=100,endpoint=True) for i in np.arange(len(start_stop_points)-1)])
+
 # if the 2D completeness update array exists as a .dcomp file load it
 if os.path.exists(path):
     print('Loading cached analytical dmag trig params from "%s".' % path)
@@ -261,33 +327,37 @@ else:
     outDict['polyFunVals'] = dict()
     outDict['error'] = dict()
     for i,j in itertools.product(np.arange(len(trigFuncs)),np.arange(nuss.shape[0])):
-        outDict['opt'][(i,j)] = minimize(polyCoeffsError, x0,args=(trigFuncs[i],nuss[j]),tol=1e-6,constraints=[{'type':'eq','fun':lambda x: trigFuncs[i](nuss[j][0])-polyFunc(x,nuss[j][0])},
+        #for i in np.arange(len(trigFuncs)):
+        #    for j in np.arange(len(start_stop_points[i])-1):
+        #DELETEnus = np.linspace(start=start_stop_points[i][j],stop=start_stop_points[i][j+1],num=100,endpoint=True)
+        outDict['opt'][(i,j)] = minimize(polyCoeffsError, x0,args=(trigFuncs[i],nuss[j]),tol=1e-8,constraints=[{'type':'eq','fun':lambda x: trigFuncs[i](nuss[j][0])-polyFunc(x,nuss[j][0])},
             {'type':'eq','fun':lambda x: trigFuncs[i](nuss[j][-1])-polyFunc(x,nuss[j][-1])},{'type':'eq','fun':lambda x: diffTrigFuncs[i](nuss[j][0])-diffPolyFunc(x,nuss[j][0])},{'type':'eq','fun':lambda x: diffTrigFuncs[i](nuss[j][-1])-diffPolyFunc(x,nuss[j][-1])}])
         outDict['nus'][(i,j)] = nuss[j]
         outDict['coeffs'][(i,j)] = outDict['opt'][(i,j)].x
         outDict['trigFunVals'][(i,j)] = trigFuncs[i](outDict['nus'][(i,j)])
         outDict['polyFunVals'][(i,j)] = polyFunc(outDict['coeffs'][(i,j)],outDict['nus'][(i,j)])
         outDict['error'][(i,j)] = outDict['opt'][(i,j)].fun
-        # if i*j %2 != 0:
-        # plt.figure()
-        # plt.plot(outDict['nus'][(i,j)],outDict['trigFunVals'][(i,j)],color='blue')
-        # plt.plot(outDict['nus'][(i,j)],outDict['polyFunVals'][(i,j)],color='red')
-        # plt.title('i: ' + str(i) + ' j: ' + str(j) + ' error: ' + str(outDict['error'][(i,j)]))
-        # plt.show(block=False)
+        if outDict['error'][(i,j)] > 1e-4:
+            plt.figure()
+            plt.plot(outDict['nus'][(i,j)],outDict['trigFunVals'][(i,j)],color='blue')
+            plt.plot(outDict['nus'][(i,j)],outDict['polyFunVals'][(i,j)],color='red')
+            plt.title('i: ' + str(i) + ' j: ' + str(j) + ' error: ' + str(outDict['error'][(i,j)]))
+            plt.show(block=False)
         print('i: ' + str(i) + ' j: ' + str(j) + ' error: ' + str(outDict['opt'][(i,j)].fun))
 
-        # store dynamic completeness array as .dcomp file
-        with open(path, 'wb') as ff:
-            pickle.dump(outDict, ff)
+    # store dynamic completeness array as .dcomp file
+    with open(path, 'wb') as ff:
+        pickle.dump(outDict, ff)
 
 
 
 #Next, I need to correlate the coefficients to c10l0 from coefficients
 #input i, w, e properly
 
-
+tstart_quart = time.time()
 quartList = list()
 for i in np.arange(len(nuss)):
+    start0 = time.time()
     quartDict = dict()
     (c_0l0, c_0l1, c_0l2, c_0l3, c_0l4) = outDict['coeffs'][(0,i)]
     (c_1l0, c_1l1, c_1l2, c_1l3, c_1l4) = outDict['coeffs'][(1,i)]
@@ -310,17 +380,23 @@ for i in np.arange(len(nuss)):
     quartDict['B'] = B
     quartDict['C'] = C
     quartDict['D'] = D
+    stop0 = time.time()
 
     #Solve Quartics
+    startquart = time.time()
     xreal, delta, P, D2, R, delta_0 = quarticSolutions_ellipse_to_Quarticipynb(A.astype('complex128'), B, C, D)
-    
+    stopquart = time.time()
+
+    start1 = time.time()
     #Create Boolean Array of solutions that are viable
-    xrealIsViable = np.logical_and(np.logical_and((np.abs(xreal.imag) < 1e-6),(xreal.real >= nuss[i][0])),(xreal.real <= nuss[i][-1])) #creates 2d array of booleans indicating if imaginary component is small and sol is within viable range
+    xrealIsViable = np.logical_and(np.logical_and((np.abs(xreal.imag) < 2.*1e-2),(xreal.real >= nuss[i][0])),(xreal.real <= nuss[i][-1])) #creates 2d array of booleans indicating if imaginary component is small and sol is within viable range
     xrealNotViable = np.logical_not(xrealIsViable)#np.logical_or(np.logical_or((np.abs(xreal.imag) > 1e-10),(np.abs(xreal.real) < nuss[i][0])), (np.abs(xreal.real) > nuss[i][-1])) #creates 2d array of booleans indicating if imaginary component is large or sol is within viable range
     xhasViable = np.any(xrealIsViable,axis=1) #checks if planet has a 'real' solution
     xhasrealInds = np.where(xhasViable == True)[0] #find planets that have at least 1 real solution
     #DELETE xreal2 = xreal + np.nan*xrealIsViable #replace all bad solutions with nan unnecessary???
+    stop1 = time.time()
 
+    start2 = time.time()
     xsols = xreal[xrealIsViable] #the viable solutions
     xInds = np.tile(np.arange(len(e)),(4,1)).T[xrealIsViable] #the index of each planet that has solutions
     quartDict['xsols'] = xsols #the true anomaly of each intersection
@@ -330,34 +406,108 @@ for i in np.arange(len(nuss)):
     quartDict['xsols2'][xrealNotViable] = xreal[xrealNotViable]*np.nan #Turn all non-viable solutions into nan
     #quartDict['beta'] = np.arccos(np.sin(np.tile(inc,(4,1)).T)*np.sin(quartDict['xsols2']+np.tile(w,(4,1)).T)) #calculate phase angle
     #quartDict['Phi'] = np.cos(quartDict['beta']/2.)**4. #calculate phase function value
-    quartDict['Phi'] = (1.+np.sin(np.tile(inc,(4,1)).T)*np.sin(quartDict['xsols2']+np.tile(w,(4,1)).T))**2./4 #TRYING THIS TO CIRCUMVENT POTENTIAL ARCCOS
+    quartDict['Phi'] = (1.+np.sin(np.tile(inc,(4,1)).T)*np.sin(quartDict['xsols2']+np.tile(w,(4,1)).T))**2./4. #TRYING THIS TO CIRCUMVENT POTENTIAL ARCCOS
     quartDict['d'] = np.tile(a.to('AU'),(4,1)).T*(1.-np.tile(e,(4,1)).T**2.)/(np.tile(e,(4,1)).T*np.cos(quartDict['xsols2'])+1.)
-    quartDict['dmags'] = deltaMag(np.tile(p,(4,1)).T,np.tile(Rp.to('AU'),(4,1)).T,quartDict['d'],quartDict['Phi']) #calculate dmag of the specified x-value
+    quartDict['dmags'] = deltaMag(np.tile(p,(4,1)).T,np.tile(Rp.to('AU'),(4,1)).T,quartDict['d'],quartDict['Phi']).real #calculate dmag of the specified x-value
 
     quartDict['dmagErrors'] = np.abs(quartDict['dmags']-dmag) #calculate error from desired value
-    quartDict['smalldmagErrors'] = quartDict['dmagErrors'] < 1e-6 #create boolean array where values are small
+    stop2 = time.time()
 
-    plt.figure(num=i)
-    #plt.hist(quartDict['dmagErrors'].flatten(),color='black')
-    plt.hist(quartDict['dmags'].flatten(),color='black',bins=50)
-    plt.yscale('log')
-    plt.title(str(i))
-    plt.show(block=False)
+    # #Plot the resulting dmags
+    # plt.figure(num=i)
+    # #plt.hist(quartDict['dmagErrors'].flatten(),color='black')
+    # plt.hist(quartDict['dmags'].flatten(),color='black',bins=200)
+    # plt.yscale('log')
+    # plt.title(str(i))
+    # plt.show(block=False)
 
+    #Keep for Nostalgia
     quartDict['xreal'] = xreal #These should be raw quartic solutions
-    quartDict['xhasrealInds'] = xhasrealInds #indicies of planets that have a viable solution in this range
+    #DELETEquartDict['xhasrealInds']= xhasrealInds #indicies of planets that have a viable solution in this range
+
+    #Remove Solutions that do not produce the desired dmag (they are obviously wrong)
+    quartDict['xrealFiltered'] = np.ones(xreal.shape)*np.nan
+    quartDict['quartDictBoolean'] = quartDict['dmagErrors'] < 5.*1e-2
+    quartDict['xrealFiltered'][quartDict['quartDictBoolean']] = quartDict['xsols2'][quartDict['quartDictBoolean']].real
+
+    quartDict['numSolsPerPlanet'] = np.sum(quartDict['quartDictBoolean'],axis=1) #calculates the number of intersection solutions each planet has
+
+    #Reducing 4 solutions down into 
 
     #save to quartList
     quartList.append(quartDict)
 
     #DELETEprint(saltyburrito)
+tstop_quart = time.time()
+
+#TOTAL numSols from cos
+print('Total Num Sols Comparison')
+print(np.sum(numSols))
+
+#Combine the viable xreal solutions
+totalNumSols = 0
+for i in np.arange(len(nuss)):
+    totalNumSols = totalNumSols + np.sum(quartList[i]['quartDictBoolean'])
+print(totalNumSols)
+
+#### Need to compare solutions between methods.
+#Are all planets with 0 solutions
+print('Num Sols Per Planet Comparison')
+numSolsPerPlanetCosine = np.sum(~np.isnan(nusCombined),axis=1)
+print(numSolsPerPlanetCosine)
+numSolsPerPlanetApprox = np.zeros(len(quartList[0]['numSolsPerPlanet']),dtype=int)
+for i in np.arange(len(nuss)):
+    numSolsPerPlanetApprox = numSolsPerPlanetApprox + quartList[i]['numSolsPerPlanet']
+print(numSolsPerPlanetApprox)
+
+#Look at planets that have no solutions
+print('Num Planets with 0 Solutions')
+print(np.sum(numSolsPerPlanetCosine == 0))
+print(np.sum(numSolsPerPlanetApprox == 0))
+print(np.sum((numSolsPerPlanetCosine == 0)*(numSolsPerPlanetApprox == 0)))
+print('Num Planets with 1 Solutions')
+print(np.sum(numSolsPerPlanetCosine == 1))
+print(np.sum(numSolsPerPlanetApprox == 1))
+print(np.sum((numSolsPerPlanetCosine == 1)*(numSolsPerPlanetApprox == 1)))
+print('Num Planets with 2 Solutions')
+print(np.sum(numSolsPerPlanetCosine == 2))
+print(np.sum(numSolsPerPlanetApprox == 2))
+print(np.sum((numSolsPerPlanetCosine == 2)*(numSolsPerPlanetApprox == 2)))
+print('Num Planets with 3 Solutions')
+print(np.sum(numSolsPerPlanetCosine == 3))
+print(np.sum(numSolsPerPlanetApprox == 3))
+print(np.sum((numSolsPerPlanetCosine == 3)*(numSolsPerPlanetApprox == 3)))
+print('Num Planets with 4 Solutions')
+print(np.sum(numSolsPerPlanetCosine == 4))
+print(np.sum(numSolsPerPlanetApprox == 4))
+print(np.sum((numSolsPerPlanetCosine == 4)*(numSolsPerPlanetApprox == 4)))
+print('Num Planets with 5 Solutions')
+print(np.sum(numSolsPerPlanetCosine == 5))
+print(np.sum(numSolsPerPlanetApprox == 5))
+print(np.sum((numSolsPerPlanetCosine == 5)*(numSolsPerPlanetApprox == 5)))
 
 #Iterate over each nu range and see how many (if any) solutions exists
 #for each nu solution create array
 #create associated array with nu-ranges
+alldmags = np.array([])
+for i in np.arange(len(quartList)):
+    alldmags = np.concatenate((alldmags,quartList[i]['dmags'].flatten()))
+plt.figure(num=10000)
+plt.hist(alldmags,color='blue',bins=200)
+plt.yscale('log')
+plt.title(str(i))
+plt.show(block=False)
 
+#Timing
+print('Timing')
+print(tstop_cos-tstart_cos)
+print(tstop_quart-tstart_quart)
 
-#Check errors
-
-#planetXsols = np.zeros((len(e),16))#np.zeros((len(e),4))
+#other timing
+print('other Timing')
+print(ttstop-ttstart) #timing for a single np roots call
+print(stop0-start0) #timing for dict generation and coeff extraction
+print(stopquart-startquart) #timing for the whole quartic process
+print(stop1-start1) #timing for some np.where bs
+print(stop2-start2) #timing for the dmag calcs
 
